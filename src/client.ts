6dcf84655235eafdbb500e212b85f6894b006d88
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-04-30 16:01:27 +0800

    src\client.ts: mcp server command is taken from env variable

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,15 @@ import { Client } from '@modelcontextprotocol/sdk/client/index.js'
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js'
 
 const transport = new StdioClientTransport( {
-  command: process.env.MCP_SERVER_COMMAND || 'node',
-  args: process.env.MCP_SERVER_ARGS ? process.env.MCP_SERVER_ARGS.split(' ') : undefined,
+  command: process.env.MCP_SERVER_COMMAND || 'npx',
+  /**
+   * args: An array of arguments passed to the command:
+   * '-y': Automatically confirms any prompts that npx might present.
+   * '@modelcontextprotocol/server-filesystem': Specifies the MCP server node package to run, which provides filesystem operations.
+   * 'absolute/path/to/allowed/dir': This is a placeholder path
+   *  Should be replaced with the absolute path to the directory on your local machine that you want the MCP server to have access to.
+   */
+  args: process.env.MCP_SERVER_ARGS ? process.env.MCP_SERVER_ARGS.split(',') : undefined,
 } )
 
 const McpClient = new Client( 
